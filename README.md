279c54a9988b6cc7d16bbc2167f63edffda876ea
Martin <martinwang@ibtool.cn>
2022-07-04 17:13:19 +0800

    :fire:  Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chrome-extension
 A Google Chrome Extension
+
+做了一个Google浏览器的插件，里面有一个方法叫`cj`，你可以用它打印json看看
+
+
+给某些你看不顺眼的网站加个边框，并标记一个徽章
--- js/popup.js
+++ js/popup.js
@@ -5,7 +5,7 @@ document.onreadystatechange = () =>{
             const id = getSelection()?.anchorNode?.parentNode?.id
             switch (id) {
                 case 'home':
-                    chrome.tabs.create({ url: "https://mr-jinhui.gitee.io/martin/"})
+                    chrome.tabs.create({ url: "https://1163895390wjh.github.io/martin/"})
                     break
                 case 'time':
                     chrome.tabs.create({ url: "pages/clock.html"})
@@ -23,6 +23,12 @@ document.onreadystatechange = () =>{
                     alert('about')
                     break
             }
+        })
+        document.querySelector('#addItem')?.addEventListener('click',function () {
+            alert('好尴尬，我还没做呢')
+        })
+        document.querySelector('#saveItem')?.addEventListener('click',function () {
+            alert('好尴尬，我还没做呢')
         })
     }
 }
